feee458b9c0c5f9dbfedf7b3f56cb9d37e00d929
Rachel Scott <99931021+r-scott25@users.noreply.github.com>
2022-12-14 15:36:56 -0600

    Update App.js

--- Exercise Files/Ch04/04_02/start/react-app/src/App.js
+++ Exercise Files/Ch04/04_02/start/react-app/src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useReducer } from "react";
 
 function App() {
+  const [checked, setChecked] = useReducer((checked) => !checked, false);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="checkbox" value={checked} onChange={setChecked} />
+      <label>{checked ? "Checked" : "Not Checked"}</label>
     </div>
   );
 }
